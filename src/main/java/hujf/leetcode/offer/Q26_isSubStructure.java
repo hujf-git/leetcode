0bfb3a09b40f8b77b6c05c8f8d78e9a6b2cdfe29
hujf.c <hujf.c@qq.com>
2022-09-20 02:07:27 +0800

    dfs and recur

--- src/main/java/hujf/leetcode/offer/Q26_isSubStructure.java
+++ src/main/java/hujf/leetcode/offer/Q26_isSubStructure.java
@@ -38,11 +38,45 @@ package hujf.leetcode.offer;
  */
 public class Q26_isSubStructure {
 
-    public boolean isSubStructure(TreeNode A, TreeNode B) {
-        return false;
+    public static void main(String[] args) {
+        TreeNode A = new TreeNode(4);
+        A.left = new TreeNode(2);
+        A.right = new TreeNode(3);
+        A.left.left = new TreeNode(4);
+        A.left.right = new TreeNode(5);
+        A.right.left = new TreeNode(6);
+        A.right.right = new TreeNode(7);
+        A.left.left.left = new TreeNode(8);
+        A.left.left.right = new TreeNode(9);
+
+        TreeNode B = new TreeNode(4);
+        B.left = new TreeNode(8);
+        B.right = new TreeNode(9);
+
+        new Q26_isSubStructure().isSubStructure(A, B);
     }
 
-    public class TreeNode {
+    /**
+     * DFS + 递归
+     */
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        if (B == null || A == null) {
+            return false;
+        }
+        return isMatch(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B);
+    }
+
+    private boolean isMatch(TreeNode A, TreeNode B) {
+        if (B == null) {
+            return true;
+        }
+        if (A == null) {
+            return false;
+        }
+        return B.val == A.val && isMatch(A.left, B.left) && isMatch(A.right, B.right);
+    }
+
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
