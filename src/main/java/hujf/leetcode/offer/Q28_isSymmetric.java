8e91ea407e3cd2aec3e9bc69d213d75911a8af23
hujf.c <hujf.c@qq.com>
2022-09-20 23:08:12 +0800

    Q28 isSymmetric

---
+++ src/main/java/hujf/leetcode/offer/Q28_isSymmetric.java
@@ -0,0 +1,79 @@
+package hujf.leetcode.offer;
+
+/**
+ * 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+ * <p>
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ * <p>
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ * <p>
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：root = [1,2,2,3,4,4,3]
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * 输入：root = [1,2,2,null,3,null,3]
+ * 输出：false
+ *  
+ * <p>
+ * 限制：
+ * <p>
+ * 0 <= 节点个数 <= 1000
+ * <p>
+ * 注意：本题与主站 101 题相同：https://leetcode-cn.com/problems/symmetric-tree/
+ * <p>
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/dui-cheng-de-er-cha-shu-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Q28_isSymmetric {
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    public boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        } else if (left != null && right == null) {
+            return false;
+        } else if (left == null && right != null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return isSymmetric(left.right, right.left) && isSymmetric(left.left, right.right);
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
