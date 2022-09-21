a594b481698e7a7aaf20a404461923c3f279285f
hujf.c <hujf.c@qq.com>
2022-09-21 23:22:38 +0800

    Q32

---
+++ src/main/java/hujf/leetcode/offer/Q32_levelOrder.java
@@ -0,0 +1,70 @@
+package hujf.leetcode.offer;
+
+import java.util.*;
+
+/**
+ * 从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+ * <p>
+ *  
+ * <p>
+ * 例如:
+ * 给定二叉树: [3,9,20,null,null,15,7],
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回：
+ * <p>
+ * [3,9,20,15,7]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 节点总数 <= 1000
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/cong-shang-dao-xia-da-yin-er-cha-shu-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Q32_levelOrder {
+
+    /**
+     * 辅助队列
+     */
+    public int[] levelOrder(TreeNode root) {
+        if (root == null) {
+            return new int[0];
+        }
+        List<Integer> ret = new ArrayList<>();
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.push(root);
+        while (!queue.isEmpty()) {
+            TreeNode tn = queue.poll();
+            ret.add(tn.val);
+            if (tn.left != null) {
+                queue.offer(tn.left);
+            }
+            if (tn.right != null) {
+                queue.offer(tn.right);
+            }
+        }
+        int[] retA = new int[ret.size()];
+        for (int i = 0; i < ret.size(); ++i) {
+            retA[i] = ret.get(i);
+        }
+        return retA;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
